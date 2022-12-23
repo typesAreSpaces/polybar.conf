70fe57acd3ae9d758ac744567d2b860ad4758d10
jose <jabelcastellanosjoo@unm.edu>
2022-12-23 01:08:10 -0700

    Add surfacego-display branch.

--- launch.sh
+++ launch.sh
@@ -10,9 +10,10 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
   #interface="$(cat /proc/net/wireless | perl -ne '/(\w+):/ && print $1')";
-  #interface="wlan0";
-  interface="enp2s0";
+  interface="wlan0";
+  #interface="enp2s0";
   NETWORK_INTERFACE=$interface MONITOR=$m polybar -c ~/.config/polybar/config.ini top_bar &
+  NETWORK_INTERFACE=$interface MONITOR=$m polybar -c ~/.config/polybar/config.ini bottom_bar &
   #NETWORK_INTERFACE=$interface MONITOR=$m polybar -c ~/.config/polybar/config.ini bottom_bar &
 done
 
