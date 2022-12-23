c65106fe06c370ae3c84dac641b493fffedbe256
jose <jabelcastellanosjoo@unm.edu>
2022-12-23 00:45:15 -0700

    First commit.

--- launch.sh
+++ launch.sh
@@ -8,11 +8,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
+#for m in $(polybar --list-monitors | cut -d":" -f1); do
   #interface="$(cat /proc/net/wireless | perl -ne '/(\w+):/ && print $1')";
-  #interface="wlan0";
-  interface="enp2s0";
-  NETWORK_INTERFACE=$interface MONITOR=$m polybar -c ~/.config/polybar/config.ini top_bar &
-  #NETWORK_INTERFACE=$interface MONITOR=$m polybar -c ~/.config/polybar/config.ini bottom_bar &
-done
+  #NETWORK_INTERFACE=$interface MONITOR=$m polybar -c ~/.config/polybar/config.ini main &
+  #done
 
+#interface="wlp59s0";
+interface="enp58s0u1u4";
+NETWORK_INTERFACE=$interface MONITOR=DP3 polybar -c ~/.config/polybar/config.ini top_bar &
+NETWORK_INTERFACE=$interface MONITOR=DP3 polybar -c ~/.config/polybar/config.ini bottom_bar &
+NETWORK_INTERFACE=$interface MONITOR=eDP1 polybar -c ~/.config/polybar/config.ini top_bar &
+NETWORK_INTERFACE=$interface MONITOR=eDP1 polybar -c ~/.config/polybar/config.ini bottom_bar &
